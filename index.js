fcba8871d7e01dff6a45f3bfd6fa6fb6788ee417
laithhanania <laith.hanania@mail.utoronto.ca>
2021-01-28 00:44:34 -0500

    Fix port capitalization

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.get('/', (req, res)=>{
     res.send({hi: 'there'});
 });
 
-const PORT = process.env.port|| 5000; //heroku production port vs local (5000) port
+const PORT = process.env.PORT|| 5000; //heroku production port vs local (5000) port
 app.listen(PORT);
+
