4570afbd3b8df5c58d450918143b9ba2745ac098
laithhanania <laith.hanania@mail.utoronto.ca>
2021-01-29 00:29:29 -0500

    removing initial route

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send({hi: 'there'});
-});
-
 //For production routing (heroku):
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
