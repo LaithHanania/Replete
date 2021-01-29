bea696bd1462e9a0f0cc3258ec8f8982ea78fcf2
laithhanania <laith.hanania@mail.utoronto.ca>
2021-01-29 00:04:12 -0500

    server side production routing

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.get('/', (req, res)=>{
     res.send({hi: 'there'});
 });
 
+//For production routing (heroku):
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT|| 5000; //heroku production port vs local (5000) port
 app.listen(PORT);
 
